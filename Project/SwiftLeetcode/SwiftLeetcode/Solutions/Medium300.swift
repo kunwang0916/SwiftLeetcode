c6dade0a246b217864980a57d1d5c5763bf3650c
kun wang <kunwangus@gmail.com>
2018-05-03 20:28:51 -0700

    Adding TODO.

--- Project/SwiftLeetcode/SwiftLeetcode/Solutions/Medium300.swift
+++ Project/SwiftLeetcode/SwiftLeetcode/Solutions/Medium300.swift
@@ -14,6 +14,7 @@
 import UIKit
 
 public class Medium300: NSObject {
+    //TODO: Adding solution in O(nLogn) time
     public class func lengthOfLIS(_ nums: [Int]) -> Int {
         return self.DP(nums)
     }
