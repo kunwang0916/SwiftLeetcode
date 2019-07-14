e7058ad160a3e232014dacd9d8d0e3c6e966068a
kun wang <kunwangus@gmail.com>
2019-07-13 18:38:04 -0700

    updated the solution 56.

--- Project/SwiftLeetcode/SwiftLeetcode/Solutions/Medium56.swift
+++ Project/SwiftLeetcode/SwiftLeetcode/Solutions/Medium56.swift
@@ -9,27 +9,32 @@
 /**
  56. Merge Intervals
  https://leetcode.com/problems/merge-intervals/
+ time: O(NLogN) sor
+ space: O(N)
  **/
 import UIKit
 
 public class Medium56: NSObject {
-    public class func merge(_ intervals: [Interval]) -> [Interval] {
-        var ints = intervals.sorted { (a, b) -> Bool in
-            a.start < b.start
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        if intervals.isEmpty {
+            return []
         }
         
-        var i = 0
-        while i < ints.count - 1 {
-            //key
-            if ints[i].end >= ints[i+1].start {
-                // tricky point
-                ints[i].end = max(ints[i].end, ints[i+1].end)
-                ints.remove(at: i+1)
+        var ints = intervals.sorted { $0[0] < $1[0] }
+        var result = [[Int]]()
+        var curInt = ints[0]
+        
+        for int in ints {
+            if int[0] <= curInt[1] {
+                curInt[1] = max(curInt[1], int[1])
             } else {
-                i += 1
+                result.append(curInt)
+                curInt = int
             }
         }
         
-        return ints
+        result.append(curInt)
+        
+        return result
     }
 }
